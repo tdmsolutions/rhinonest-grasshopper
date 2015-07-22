e883c8a0adf9ef7f769f2f4e8deb395f4a0d94b8
Rafael del Molino <rafael@tdmsolutions.com>
2015-07-22 08:42:25 +0200

    Starting Project, let's start!

---
+++ src/RhinoNestForGrasshopper/RhinoNestForGrasshopperInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace RhinoNestForGrasshopper
+{
+    public class RhinoNestForGrasshopperInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "RhinoNestForGrasshopper";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("14fdb4d2-ead5-46c9-bb5b-294765da6ff3");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "TDM Solutions SLU";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "info@tdmsolutions.com";
+            }
+        }
+    }
+}
